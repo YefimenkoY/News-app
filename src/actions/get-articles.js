7369937b7f924a74edc496882245df78b3c655ce
ibol34t1 <ibol34tt@gmail.com>
2017-08-24 22:06:27 +0300

    refactoring

--- src/actions/get-articles.js
+++ src/actions/get-articles.js
@@ -1,14 +1,20 @@
+import { bindActionCreators } from 'redux';
 import { get } from '../api';
 import { getArticles } from '../api/end-points';
 import STATUSES from '../constants/statuses';
 import AC from './';
 
 export const fetchArticles = params => async dispatch => {
+  const {
+    saveArticles
+  } = bindActionCreators({
+    saveArticles: AC.saveArticles,
+  }, dispatch);
   const { data, statusText } = await get(
     getArticles(),
     params
   );
   
   if (statusText === STATUSES.STATUS_OK)
-    dispatch(AC.saveArticles(data.articles));
+    saveArticles(data.articles);
 };
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,6 @@ import { fetchSources } from './get-sources';
 import { fetchArticles } from './get-articles';
 import AT from './types';
 
-console.log('========');
-console.log(AT);
-console.log('========');
-
 export default {
   
   saveArticles: articles => (
