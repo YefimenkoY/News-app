6f72dc25143dfad12da7c439bc7670195eef5583
ibol34t1 <ibol34tt@gmail.com>
2017-09-18 19:05:34 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-import App from './containers/app';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from '../src/store';
 import reducers from './reducers';
 import middlewares from './middlewares';
+import createRoutes from '../src/routes';
 
-export default { App, reducers, middlewares };
-export { App, reducers, middlewares };
+import '../src/styles/main.scss';
+
+const store = configureStore(reducers, middlewares);
+const routes = createRoutes();
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>,
+  app
+);
--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,25 @@
 const path = require('path');
 const webpack = require('webpack');
 const utils = require('./webpack/utils');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const LOCAL_ENV = 'local';
 const DEV_ENV = 'development';
 const PROD_ENV = 'production';
 
-const BUILD_FOLDER = './build';
-const PUBLIC_FOLDER = './build/js';
+const BUILD_FOLDER = './src';
+const PUBLIC_FOLDER = './build';
 
 const NODE_ENV = process.env.NODE_ENV || DEV_ENV;
 const APP_FOLDER = process.cwd();
 const BUILD_FILE = './index.js';
+const TEMPLATE_FILE = './index.html';
 
 const config = {
   entry: path.join(APP_FOLDER, BUILD_FOLDER, BUILD_FILE),
   output: {
     path: path.join(APP_FOLDER, PUBLIC_FOLDER),
-    publicPath: 'js/',
     filename: 'bundle.js'
-  },
-  externals: {
-    'react': 'React',
-    'redux': 'Redux',
-    'react-dom': 'ReactDOM',
-    'react-redux': 'ReactRedux',
-    'config': JSON.stringify(require('./build-config'))
   },
   watch: NODE_ENV === LOCAL_ENV,
   devtool: NODE_ENV === DEV_ENV ? 'source-map' : false,
@@ -37,18 +31,13 @@ const config = {
       'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
       DEV_ENV: JSON.stringify(DEV_ENV)
     }),
+    new HtmlWebpackPlugin({
+      template: path.join(APP_FOLDER, BUILD_FOLDER, TEMPLATE_FILE),
+    }),
   ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/, options: {
-        babelrc: false,
-        presets: ['es2015', 'stage-0', 'react'],
-        plugins: [
-          'transform-decorators-legacy',
-          'transform-class-properties',
-          'transform-runtime'
-        ]
-      } },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
         test: /\.css$/,
         loader: ['style-loader', 'css-loader?modules', 'sass-loader?modules']
@@ -61,7 +50,11 @@ const config = {
         test: /\.scss$/,
         loader: ['style-loader', 'css-loader', 'sass-loader']
       },
-      { test: /\.json$/, loader: 'json-loader' }
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.(html)$/,
+        use: { loader: 'html-loader' }
+      }
     ]
   },
   devServer: {
@@ -77,8 +70,6 @@ const config = {
 if (NODE_ENV === PROD_ENV) {
   config.plugins.push(utils.Uglify());
   config.plugins.push(utils.GZip());
-  config.module.loaders[0].options.presets.push('react-optimize');
 }
 
 module.exports = config;
-
